bb502fdb0b705edeee79cd295d5b4d5fe454ddf5
Carmelo Vicente Huesca Calatayud <carmelovicente.huesca@aqualogybs.net>
2017-01-09 12:18:57 +0100

    incluir dependecia de jsdoc

--- src/Bazinga.js
+++ src/Bazinga.js
@@ -1,58 +1,57 @@
 var Bazinga = function(number) {
-
-    this.setNumber = function(number) {
-        var number = parseInt(number);
-        var isValid = (number !== NaN && number > 0);
-        this.number = isValid ? number : 1;
-        this.result = isValid ? number.toString() : 'entrada incorrecta';
-    };
-
-    this.isMultipleOf = function(multiple) {
-        return (this.number % multiple === 0);
-    };
-
-    this.setResult = function(logic, text) {
-        this.result = (logic) ? text : this.result;
-    };
-
-    this.isPenny = function() {
-        var logic = this.isMultipleOf(3);
-        this.setResult(logic, 'Penny');
-        return logic;
-    };
-
-    this.isSheldon = function() {
-        var logic = this.isMultipleOf(5);
-        this.setResult(logic, 'Sheldon');
-        return logic;
-    };
-
-    this.isLeonard = function() {
-        var logic = (this.isPenny() && this.isSheldon());
-        this.setResult(logic, 'Leonard');
-        return logic;
-    };
-
-    this.isHoward = function() {
-        var logic = (this.isLeonard() && !this.isMultipleOf(2));
-        this.setResult(logic, 'Howard');
-        return logic;
-    };
-
-    this.isBazinga = function() {
-        var logic = (this.isHoward() && this.isMultipleOf(7));
-        this.setResult(logic, 'Bazinga');
-        return logic;
-    };
-
-    this.run = function() {
-        this.isPenny();
-        this.isSheldon();
-        this.isLeonard();
-        this.isHoward();
-        this.isBazinga();
-        return this.result;
-    };
-
     this.setNumber(number);
+};
+
+Bazinga.prototype.setNumber = function(number) {
+    var number = parseInt(number);
+    var isValid = (number !== NaN && number > 0);
+    this.number = isValid ? number : 1;
+    this.result = isValid ? number.toString() : 'entrada incorrecta';
+};
+
+Bazinga.prototype.isMultipleOf = function(multiple) {
+    return (this.number % multiple === 0);
+};
+
+Bazinga.prototype.setResult = function(logic, text) {
+    this.result = (logic) ? text : this.result;
+};
+
+Bazinga.prototype.isPenny = function() {
+    var logic = this.isMultipleOf(3);
+    this.setResult(logic, 'Penny');
+    return logic;
+};
+
+Bazinga.prototype.isSheldon = function() {
+    var logic = this.isMultipleOf(5);
+    this.setResult(logic, 'Sheldon');
+    return logic;
+};
+
+Bazinga.prototype.isLeonard = function() {
+    var logic = (this.isPenny() && this.isSheldon());
+    this.setResult(logic, 'Leonard');
+    return logic;
+};
+
+Bazinga.prototype.isHoward = function() {
+    var logic = (this.isLeonard() && !this.isMultipleOf(2));
+    this.setResult(logic, 'Howard');
+    return logic;
+};
+
+Bazinga.prototype.isBazinga = function() {
+    var logic = (this.isHoward() && this.isMultipleOf(7));
+    this.setResult(logic, 'Bazinga');
+    return logic;
+};
+
+Bazinga.prototype.run = function() {
+    this.isPenny();
+    this.isSheldon();
+    this.isLeonard();
+    this.isHoward();
+    this.isBazinga();
+    return this.result;
 };
