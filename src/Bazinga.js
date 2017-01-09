0f891dd38694b9b42c68f3752e714cc3129e35c0
Carmelo Vicente Huesca Calatayud <carmelovicente.huesca@aqualogybs.net>
2017-01-09 10:42:37 +0100

    Hay que desarrollar un juego llamado Bazinga

---
+++ src/Bazinga.js
@@ -0,0 +1,1 @@
+var Bazinga = function(){};
---
+++ test/Bazinga.spec.js
@@ -0,0 +1,36 @@
+describe('Bazinga:', function() {
+
+	it('Hay que desarrollar un juego llamado Bazinga', function() {
+		expect(new Bazinga()).toBeDefined();
+	});
+
+	it('El constructor tiene de entrada un número entero mayor que 0', function() {
+
+	});
+
+	it('tiene un método run() que devuelve el resultado en formato string', function() {
+
+	});
+
+	it('si el número es divisible por 3 debe devolver "Penny"', function() {
+
+	});
+
+	it('si el número es divisible por 5 debe devolver "Sheldon"', function() {
+
+	});
+
+	it('si el número es divisible por 3 y 5 debe devolver "Leonard"', function() {
+
+	});
+
+	it('si el número es divisible por 3 y 5 pero no por 2 debe devolver "Howard"  ', function() {
+
+	});
+
+	it('si el número es divisible por 3, 5 y 7 pero no por 2 debe devolver "Bazinga"', function() {
+
+	});
+
+});
+
