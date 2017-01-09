0534a10d30a841fb4cff6b8c06037e029096cf98
Carmelo Vicente Huesca Calatayud <carmelovicente.huesca@aqualogybs.net>
2017-01-09 12:39:37 +0100

    documentación de Bazinga

--- src/Bazinga.js
+++ src/Bazinga.js
@@ -1,57 +1,100 @@
+/**
+ * Juego TDD Bazinga
+ * @constructor
+ * @class Bazinga
+ * @param {number} number número para la lógica
+ */
 var Bazinga = function(number) {
     this.setNumber(number);
 };
-
+/**
+ * Setea el valor del número
+ * @method Bazinga.setNumber
+ * @param {number} number número para la lógica
+ */
 Bazinga.prototype.setNumber = function(number) {
     var number = parseInt(number);
     var isValid = (number !== NaN && number > 0);
     this.number = isValid ? number : 1;
     this.result = isValid ? number.toString() : 'entrada incorrecta';
 };
-
+/**
+ * Método que devuelve si un número es múltiplo
+ * @param  {number}  multiple número múltiplo
+ * @return {boolean}          Resultado de la operación
+ */
 Bazinga.prototype.isMultipleOf = function(multiple) {
     return (this.number % multiple === 0);
 };
-
+/**
+ * Establece el resultado si ha ido bien la operacion lógica
+ * @method Bazinga#setResult
+ * @param {boolean} logic operación lógica
+ * @param {string} text  texto del resultado
+ */
 Bazinga.prototype.setResult = function(logic, text) {
     this.result = (logic) ? text : this.result;
 };
-
+/**
+ * Lógica para el resultado de "Penny"
+ * @method Bazinga#isPenny  
+ * @return {boolean} Resultado de la lógica
+ */
 Bazinga.prototype.isPenny = function() {
-    var logic = this.isMultipleOf(3);
+    let logic = this.isMultipleOf(3);
     this.setResult(logic, 'Penny');
     return logic;
 };
-
+/**
+ * Lógica para el resultado de "Sheldon"
+ * @method Bazinga#isSheldon    
+ * @return {boolean} Resultado de la lógica
+ */
 Bazinga.prototype.isSheldon = function() {
-    var logic = this.isMultipleOf(5);
+    let logic = this.isMultipleOf(5);
     this.setResult(logic, 'Sheldon');
     return logic;
 };
-
+/**
+ * Lógica para el resultado de "Leonard"
+ * @method Bazinga#isLeonard    
+ * @return {boolean} Resultado de la lógica
+ */
 Bazinga.prototype.isLeonard = function() {
-    var logic = (this.isPenny() && this.isSheldon());
+    let logic = this.isPenny() && this.isSheldon();
     this.setResult(logic, 'Leonard');
     return logic;
 };
-
+/**
+ * Lógica para el resultado de "Howard"
+ * @method Bazinga#isHoward     
+ * @return {boolean} Resultado de la lógica
+ */
 Bazinga.prototype.isHoward = function() {
-    var logic = (this.isLeonard() && !this.isMultipleOf(2));
+    let logic = this.isLeonard() && !this.isMultipleOf(2);
     this.setResult(logic, 'Howard');
     return logic;
 };
-
+/**
+ * Lógica para el resultado de "Bazinga"
+ * @method Bazinga#isBazinga    
+ * @return {boolean} Resultado de la lógica
+ */
 Bazinga.prototype.isBazinga = function() {
-    var logic = (this.isHoward() && this.isMultipleOf(7));
+    let logic = this.isHoward() && this.isMultipleOf(7);
     this.setResult(logic, 'Bazinga');
     return logic;
 };
-
+/**
+ * Método que se encarga de ejecutar todas las lógicas
+ * @method Bazinga#run  
+ * @return {string} Resultado del juego
+ */
 Bazinga.prototype.run = function() {
     this.isPenny();
     this.isSheldon();
     this.isLeonard();
     this.isHoward();
-    this.isBazinga();
+    this.isBazinga()
     return this.result;
 };
